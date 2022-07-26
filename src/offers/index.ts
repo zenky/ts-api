8f18dfbcef128e20e872bcfa47bfb37f440098aa
Timur Zurbaev <zurbaev@gmail.com>
2022-07-26 16:22:34 +0800

    Export offers types

--- src/offers/index.ts
+++ src/offers/index.ts
@@ -3,6 +3,8 @@ import { PaginatedResponse, ResourceRequest } from '../types.js';
 import { getStoreUrl, usePaginationRequestWrapper, useRequestWrapper } from '../api.js';
 import { AxiosInstance } from 'axios';
 
+export * from './types.js';
+
 export async function getOffers(params: OffersPaginationRequest): Promise<PaginatedResponse<Offer>> {
   return usePaginationRequestWrapper<Offer>(async function (axios: AxiosInstance, storeId: string) {
     return axios.get(getStoreUrl(storeId, '/offers'), { params });
