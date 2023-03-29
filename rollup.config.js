ba004e19faee5b3b8bdc5020e397d26d9544e763
tzurbaev <zurbaev@gmail.com>
2023-03-29 15:48:33 +0800

    Update package.json

--- rollup.config.js
+++ rollup.config.js
@@ -6,11 +6,11 @@ export default [
     output: [
       {
         format: 'esm',
-        dir: 'dist/esm',
+        file: 'dist/esm/index.mjs',
       },
       {
         format: 'cjs',
-        dir: 'dist/cjs',
+        file: 'dist/cjs/index.cjs',
       },
     ],
     external: [
